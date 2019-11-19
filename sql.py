e2c871465cd07ef37ca214ece1ac1f8e30b45e7f
MartinMwiti <martinmwiti007@gmail.com>
2019-11-19 23:52:37 +0300

    updated sql file

--- sql.py
+++ sql.py
@@ -1,3 +1,4 @@
+import datetime
 import mysql.connector
 #INITIALIZING
 db = mysql.connector.connect(
@@ -19,7 +20,7 @@ mycursor.execute("CREATE DATABASE testdatabase")  # written first before adding 
 
 mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
 #used "smallint" instead of "int" to allow respresentation of smaller amount of bits. unsigned to remove any signs sice age isn't a negative number
-        #VIEW DATABASE
+        #VIEW DATABASE TABLE
 mycursor.execute("DESCRIBE Person")
 
 for x in mycursor: #get all output got from table "Person" and print line by line
@@ -40,5 +41,49 @@ for x in mycursor:
 """
 
 #CREATE SECOND TABLE
-'''mycursor.execute("CREATE TABLE Test(name VARCHAR(50) NOT NULL, created datetime NOT NULL, gender ENUM('M', 'F', 'O') NOT NULL, id int PRIMARY KEY NOT NULL AUTO_INCREMENT)")'''
+'''
+mycursor.execute("CREATE TABLE Test(name VARCHAR(50) NOT NULL, created datetime NOT NULL, gender ENUM('M', 'F', 'O') NOT NULL, id int PRIMARY KEY NOT NULL AUTO_INCREMENT)")
+    #VIEW DATABASE TABLE
+mycursor.execute("DESCRIBE Test")
 
+for x in mycursor:
+    print(x)
+
+#INSERTING VALUES TO THE TABLE
+mycursor.execute("INSERT INTO Test (name, created, gender) VALUES(%s,%s,%s)",
+                 ("Tim", datetime.datetime.now(), "M"))
+db.commit()
+    #VIEW TABLE CONTENTS USING CONDITIONS
+mycursor.execute("SELECT * FROM Test WHERE gender = 'M' ")
+for x in mycursor:
+    print(x)
+
+mycursor.execute("SELECT * FROM Test WHERE gender = 'M' ORDER BY id DESC")
+for x in mycursor:
+    print(x)
+    
+
+mycursor.execute("SELECT id, name FROM Test WHERE gender = 'M' ORDER BY id ASC")
+for x in mycursor:
+    print(x)
+
+#ALTER TABLE
+mycursor.execute("ALTER TABLE Test ADD COLUMN city VARCHAR(50) NOT NULL")
+    #DROP FROM TABLE
+mycursor.execute("ALTER TABLE Test DROP city")   
+
+mycursor.execute('DESCRIBE Test')
+print(mycursor.fetchone) #only if you have one line of input
+
+for x in mycursor:
+    print(x)
+
+    #CHANGE NAME OF OUR COLUMNS OR TABLE
+
+mycursor.execute('ALTER TABLE Test CHANGE name first_name VARCHAR(50)') #change column name from "name" to "first_name"
+mycursor.execute('DESCRIBE Test')
+
+for x in mycursor:
+    print(x)
+
+'''
